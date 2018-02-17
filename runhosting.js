8dcf5c6a9d51457346c13436fb1be84da1e723f7
zaoqi <zaomir@outlook.com>
2018-02-17 13:13:16 +0800

    .

--- runhosting.js
+++ runhosting.js
@@ -13,7 +13,6 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (function(){
-
 var main=function(){
 
 console.log('!');
@@ -24,9 +23,6 @@ if(tbl_1_row_1_input==null) {
 	console.log(tbl_1_row_1_input.check);
 }
 
-
 };
-
 setTimeout(main, 0);
-
-});
+})();
